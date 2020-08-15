38198e4d0fbc17f81a34ffa17f231d6db1e7d078
oleg_vasilenok <oleg_vasilenok@epam.com>
2020-08-16 00:17:44 +0300

    Add messageTtl

--- subscribeToExchange.js
+++ subscribeToExchange.js
@@ -4,18 +4,19 @@ module.exports = async function subscribeToExchange(channel, {
   exchangeName,
   exchangeType,
   queueName,
+  messageTtl,
   routingKey,
   autoAck = false
 }, handleEvent) {
 
   await channel.assertExchange(exchangeName, exchangeType, {durable: true});
-  const queue = (await channel.assertQueue(queueName, {exclusive: false, messageTtl: 10000})).queue;
+  const queue = (await channel.assertQueue(queueName, {exclusive: false, messageTtl})).queue;
   await channel.bindQueue(queue, exchangeName, routingKey);
   return channel.consume(queue, async (msg) => {
     if (msg !== null) {
       try {
         const newEvent = JSON.parse(msg.content.toString());
-        console.log('New event received:', newEvent);
+        console.trace(`New message received in queue '${queue}'`, newEvent);
         return await handleEvent(newEvent, () => {
           channel.ack(msg);
         });
