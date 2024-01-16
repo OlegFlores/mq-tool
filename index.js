ce0cffe2f1160350fd22cb91093addbea2158b01
Oleg Flores <431502+OlegFlores@users.noreply.github.com>
2024-01-16 01:44:24 +0100

    Export MQDelayedPublisher, bump the version

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   establishMQChannel: require('./establishMQChannel'),
   subscribeToExchange: require('./subscribeToExchange'),
   MQPublisher: require('./MQPublisher'),
+  MQDelayedPublisher: require('./MQDelayedPublisher'),
   MQExchange: require('./MQExchange')
 }
